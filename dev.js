e246415a7a73031c54e5722c47d9698b481e09bc
firchtn <firaschaabencss@gmail.com>
2016-10-25 09:18:07 +0100

    add all file of Dev Space organization

---
+++ dev.js
@@ -0,0 +1,6 @@
+document.getElementById("inlineRadio56").onchange = function(e) {
+
+console.log(document.getElementById("inlineRadio56").checked);
+	if (document.getElementById("inlineRadio56").checked)
+		{document.getElementById("myButton").disabled = false;}
+}
---
+++ stage.js
@@ -0,0 +1,9 @@
+$("#web").click(function(){
+    alert("Creation de site web \n Creation de application web");
+}); 
+$("#lunix").click(function(){
+    alert("adminestration Lunix \n Programation bash \n ");
+}); 
+$("#game").click(function(){
+    alert("Creation des jeux 3D");
+});
